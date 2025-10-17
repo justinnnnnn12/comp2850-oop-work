2ba9fac39e7240746124453a4ca1530b76acea26
justinnnnnn12 <justincwh12@gmail.com>
2025-10-17 17:27:16 +0000

    done for main.kt

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,2 +1,21 @@
 fun main() {
+    val words = readWordList("data/words.txt")
+    val target = pickRandomWord(words)
+    var success = false
+
+    println("Welcome to Wordle!")
+    println("Try to guess the 5-letter word!")
+
+    for(attempt in 1..10) {
+        val guess = obtainGuess(attempt)
+        val result = evaluateGuess(guess, target)
+        displayGuess(guess, result)
+
+        if (result.all { it == 1}) {
+            println("you guesses it! The word was $target")
+            success = true
+            break
+        }
+    }
 }
+if (!sucess) println("Out of attempts! The word was $target")
