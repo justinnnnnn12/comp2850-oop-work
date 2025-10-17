0e43c7045ed70b3ee97bd087a88612d06645bf70
justinnnnnn12 <justincwh12@gmail.com>
2025-10-17 23:54:55 +0000

    wordle part done

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,1 +1,40 @@
 // Implement the six required functions here
+import java.io.File
+
+fun isValid(word: String): Boolean {
+    return word.length == 5 && word.all { it.isLetter()}
+}
+
+fun readWordList(filename: String): MutableList<String> {
+    return File(filename)
+    .readLines()
+    .map { it.trim().uppercase() }
+    .filter { isValid(it) }
+    .toMutableList()
+}
+
+fun pickRandomWord(words: MutableList<String>): String {
+    val index = (0 until words.size).random()
+    return words.removeAt(index)
+}
+
+fun obtainGuess(attempt: Int): String {
+    while (true) {
+        print("Attempt $attempt: ")
+        val guess = readln().trim().uppercase()
+        if (isValid(guess)) return guess
+        println("Invalid word! Please enter exactly 5 letters.")
+    }
+}
+
+fun evaluateGuess(guess: String, target: String): List<Int> {
+    return guess.mapIndexed {i, c -> if (c ==target[i] 1 else 0)}
+}
+
+fun displayGuess(guess: String, matches: List<Int>) {
+    for(i in guess.indices) {
+        if(matches[i] == 1) print(guess[i])
+        else print('?')
+    }
+    println()
+}
