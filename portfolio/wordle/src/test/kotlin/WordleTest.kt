8b8304f894d30ab81f5dd3d3e871b0d2939837cd
justinnnnnn12 <justincwh12@gmail.com>
2025-10-22 22:44:13 +0000

    done with WordleTest

--- portfolio/wordle/src/test/kotlin/WordleTest.kt
+++ portfolio/wordle/src/test/kotlin/WordleTest.kt
@@ -4,5 +4,29 @@ import io.kotest.matchers.shouldBe
 
 @Suppress("unused")
 class WordleTest : StringSpec({
-    // Write your tests here
-})
+    //for isValid() 
+    "words that are valid should be 5 letters long"{
+    isValid("APPLE") shouldBe(true)
+    isValid("DOG") shouldBe(false)
+    isValid("HELLO!") shouldBe(false)
+    }
+
+    "should read words from file and only keep valid ones"{
+    val words = readWordList("data/words.txt")
+    words.all { it.length == 5 } shouldBe true
+    }
+
+    "should pick a random word and remove it from the list"{
+        val list = mutableListOf("APPLE", "BERRY", "CHAIR")
+        val word = pickRandomWord(list)
+        list.contains(word) shouldBe false
+
+    }
+
+    "should give correct letters 1 and others 0"{
+        val result = evaluateGuess("APPLE", "APRON")
+        result shouldBe listOf(1, 1, 0, 0, 0)
+    }
+}
+
+)
